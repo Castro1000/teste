6eb5b1ba7497e2c6e07437db2aab526dd1ab72de
ivaney <ivaneicastro2@gmail.com>
2024-05-31 10:30:02 -0400

    atualiza

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 3306;
+const port = 3306; // Porta do servidor Node.js
 
 app.use(cors());
 app.use(express.json());
 
 // Configuração do banco de dados
 const db = mysql.createConnection({
-  host: '473702ab5104.sn.mynetname.net',
+  host: '192.168.1.197', // Atualize com seu novo host, se necessário
   user: 'root',
   password: '',
   database: 'sgveauto',
@@ -25,10 +25,11 @@ db.connect(err => {
   console.log('Conectado ao banco de dados MySQL');
 });
 
-// Rota para obter os usuários
-app.get('/usuarios', (req, res) => {
-  const query = 'SELECT nome, login FROM usuario';
-  db.query(query, (err, results) => {
+// Rota para buscar produtos
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+  const sql = `SELECT Descricao, Preco, Quantidade FROM produto WHERE Descricao LIKE ?`;
+  db.query(sql, [`%${query}%`], (err, results) => {
     if (err) {
       console.error('Erro ao buscar os dados:', err);
       res.status(500).send('Erro ao buscar os dados');
