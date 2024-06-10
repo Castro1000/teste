b240c77e2204063dfa87cba6adc0646262875e3f
ivaney <ivaneicastro2@gmail.com>
2024-06-10 13:40:59 -0400

    novaaaaa

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 
-const SearchResults = ({ results }) => {
+const SearchResults = ({ results, addToCart }) => {
   return (
     <div className="search-results">
       <h2>RESULTADOS DA PESQUISA:</h2>
@@ -10,6 +10,7 @@ const SearchResults = ({ results }) => {
           <p><strong>Descrição:</strong> {result.Descricao}</p>
           <p><strong>Preço:</strong> R${result.Preco}</p>
           <p><strong>Quantidade em Estoque:</strong> {result.Quantidade}</p>
+          <button onClick={() => addToCart(result)}>Adicionar ao Carrinho</button>
           <hr />
         </div>
       ))}
